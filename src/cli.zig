04715a1bf053fa6976caa028a714cc5925951b82
npc1054657282 <ly1054657282@gmail.com>
2025-07-17 05:54:38 +0800

    feat: 实现共享参数共通的初始化逻辑 除此之外，还做了两项改动。 第一项是把编译时不会变的常量修改为了容器内的常量声明而非一个函数。 常量声明明确了这是不动的，编译期计算完了就不会变了。 而原先的`getCmd()`函数有可能给人以“这是获取一个动态变化内容”的错误暗示。 此外，去除了对`@This()`的频繁使用，参见学习文档。

--- src/cli.zig
+++ src/cli.zig
@@ -2,32 +2,41 @@ const std = @import("std");
 const zargs = @import("zargs");
 pub const Runner = union(enum) {
     prep: @import("prep/prep_cli.zig").Prep,
-    pub fn run(self: *@This()) !void {
-        switch (self.*) {
-            inline else => |*case| return case.run(),
-        }
-    }
-    pub fn getCmd() zargs.Command {
-        comptime var cmd = zargs.Command.new("gvca").requireSub("sub")
+    const cmd = cmd_blk: {
+        var building_cmd = zargs.Command.new("gvca").requireSub("sub")
             .about("git version commit aligner")
             .version("0.0.0")
             .author("npc1054657282");
-        inline for (@typeInfo(Runner).@"union".fields) |field| {
-            cmd = cmd.sub(field.type.getCmd());
+        for (@typeInfo(Runner).@"union".fields) |field| {
+            building_cmd = building_cmd.sub(field.type.cmd);
         }
-        return cmd;
+        break :cmd_blk building_cmd;
+    };
+    pub const Global = struct {
+        verbose: bool,
+    };
+    // 为子命令添加全局共享参数。不是指那种必须在子命令前输入的全局参数，我不打算使用此类参数。此处是每个子命令都会指定重复添加的参数。
+    pub fn sharedArgs(sub_cmd: zargs.Command) zargs.Command {
+        return sub_cmd.arg(zargs.Arg.opt("verbose", bool).short('v').long("verbose"));
     }
-    // 全局共享参数。不是指那种必须在子命令前输入的参数，我不打算使用此类参数。此处是每个子命令都会指定重复添加的参数。
-    pub fn sharedArgs(cmd: zargs.Command) zargs.Command {
-        return cmd.arg(zargs.Arg.opt("verbose", bool).short('v').long("verbose"));
+    pub fn initGlobal(args: anytype) Global {
+        std.debug.assert(@hasField(@TypeOf(args), "verbose"));
+        return .{
+            .verbose = args.verbose,
+        };
     }
-    pub fn initFromArgs(args: @This().getCmd().Result(), allocator: std.mem.Allocator) @This() {
+    pub fn initFromArgs(args: Runner.cmd.Result(), allocator: std.mem.Allocator) Runner {
         // 这里可以插入处理全局参数。但目前我的范式是不使用名义上的全局参数，而是将全局参数变为所有子命令都共同使用一份的“共享参数”
         // 因此这块全局参数的处理逻辑不实现。
         switch (args.sub) {
             inline else => |subarg, subtag| {
-                return @FieldType(@This(), @tagName(subtag)).initFromArgs(subarg, allocator);
+                return @FieldType(Runner, @tagName(subtag)).initFromArgs(subarg, allocator);
             },
+        }
+    }
+    pub fn run(self: *Runner) !void {
+        switch (self.*) {
+            inline else => |*case| return case.run(),
         }
     }
 };
@@ -36,11 +45,11 @@ pub const Runner = union(enum) {
 // 主要原因是zargs会自动创建一个解析结果，我们不希望这个解析结果占用整个程序的生命周期。
 // 因此，将它包装在一个函数里，将解析结果转化为一个执行器，这样这个解析结果的生命周期就可以在执行完以后提前结束了。
 pub fn parseArgs(allocator: std.mem.Allocator) Runner {
-    const cmd = Runner.getCmd();
+    const cmd = Runner.cmd;
     const args = cmd.parse(allocator) catch |e|
         zargs.exitf(e, 1, "\n{s}\n", .{cmd.usage()});
     defer cmd.destroy(&args, allocator);
-    var runner: @import("cli.zig").Runner = undefined;
+    var runner: Runner = undefined;
     runner = Runner.initFromArgs(args, allocator);
     return runner;
 }
--- src/prep/prep_cli.zig
+++ src/prep/prep_cli.zig
@@ -2,22 +2,20 @@ const std = @import("std");
 const zargs = @import("zargs");
 const cli = @import("../cli.zig");
 pub const Prep = struct {
-    verbose: bool,
+    global: cli.Runner.Global,
     bare_repo_path: []const u8,
-    pub fn run(self: *@This()) !void {
-        std.debug.print("verbose: {}, path: {s}", .{ self.verbose, self.bare_repo_path });
+    pub const cmd = cli.Runner.sharedArgs(zargs.Command.new("prep"))
+        .arg(zargs.Arg.optArg("repo_path", ?[]const u8).long("repo-path"));
+    pub fn run(self: *Prep) !void {
+        std.debug.print("verbose: {}, path: {s}", .{ self.global.verbose, self.bare_repo_path });
         return;
     }
-    pub fn getCmd() zargs.Command {
-        return cli.Runner.sharedArgs(zargs.Command.new("prep"))
-            .arg(zargs.Arg.optArg("repo_path", ?[]const u8).long("repo-path"));
-    }
-    pub fn initFromArgs(args: @This().getCmd().Result(), allocator: std.mem.Allocator) cli.Runner {
+    pub fn initFromArgs(args: Prep.cmd.Result(), allocator: std.mem.Allocator) cli.Runner {
         const bare_repo_path = allocator.alloc(u8, (args.repo_path orelse "").len) catch |e| {
             std.debug.print("{}", .{e});
             std.process.abort();
         };
         @memcpy(bare_repo_path, args.repo_path orelse "");
-        return .{ .prep = .{ .verbose = args.verbose, .bare_repo_path = bare_repo_path } };
+        return .{ .prep = .{ .global = cli.Runner.initGlobal(args), .bare_repo_path = bare_repo_path } };
     }
 };
